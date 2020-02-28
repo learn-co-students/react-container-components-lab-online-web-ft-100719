ff0c45a3b62f13d8509fc09616d5f01e0b2bb7e3
jesselsmith <jesse.lee.smith@gmail.com>
2020-02-27 22:04:04 -0800

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,27 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+  state = {
+    reviews: []
+  }
+
+  componentDidMount(){
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(json => {
+      this.setState({
+        reviews: json.results
+      })
+    })
+  }
+
+  render(){
+    return(
+      <div className='latest-movie-reviews'>
+        <h3>Latest Movie Reviews</h3>
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -4,6 +4,34 @@ import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+            + `api-key=${NYT_API_KEY}&query=`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+  state = {
+    reviews: [],
+    searchTerm: ''
+  }
+
+  handleSearchTermChange = e => this.setState({searchTerm: e.target.value})
+
+  handleSearch = e => {
+    e.preventDefault()
+    fetch(URL + this.state.searchTerm)
+    .then(resp => resp.json())
+    .then(json => this.setState({reviews: json.results}))
+  }
+
+  render(){
+    return(
+      <div className='searchable-movie-reviews'>
+        <form onSubmit={this.handleSearch}>
+          <input type='text' value={this.state.searchTerm} onChange={this.handleSearchTermChange}/>
+          <input type='submit' value='Search Reviews'/>
+        </form>
+        <h3>Search Results:</h3>
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
+}
